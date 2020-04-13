fb46bf6b11013fbbed67ca7e1a584c9e201aa95f
fubblea <ajay.arthur@gmail.com>
2020-04-13 12:06:26 +0400

    Implemented Player Stats

---
+++ Sapling FPS/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int hp;
+    public int maxHp;
+
+    public int stamina;
+    public int maxStamina;
+
+    public bool isDead = false;
+
+    private void CheckStamina()
+    {
+        if (stamina >= maxStamina)
+        {
+            stamina = maxStamina;
+        }
+
+        if (stamina <= 0)
+        {
+            stamina = 0;
+        }
+    }
+
+    private void CheckHealth()
+    {
+        if (hp >= maxHp)
+        {
+            hp = maxHp;
+        }
+
+        if (hp <= 0)
+        {
+            hp = 0;
+            isDead = true;
+        }
+    }
+
+    public virtual void Die()
+    {
+        //Override
+    }
+}
---
+++ Sapling FPS/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+
+    private void Start()
+    {
+        maxHp = 100;
+        hp = maxHp;
+
+        maxStamina = 100;
+        stamina = maxStamina;
+    }
+
+    public override void Die()
+    {
+        Debug.Log("You ded");
+    }
+}
