3b64bf2c6498be36fcb81ac4432e98b688d294e3
fubblea <ajay.arthur@gmail.com>
2020-04-13 11:35:18 +0400

    Implemented Jump

--- Sapling FPS/Assets/Scripts/PlayerMotor.cs
+++ Sapling FPS/Assets/Scripts/PlayerMotor.cs
@@ -9,6 +9,7 @@ public class PlayerMotor : MonoBehaviour
     [SerializeField] float gravity = 6;
     [SerializeField] float walkSpeed = 4;
     [SerializeField] float sprintSpeed = 6;
+    [SerializeField] float jumpSpeed = 40;
 
     float moveSpeed;
     Vector3 moveDirection;
@@ -35,17 +36,31 @@ public class PlayerMotor : MonoBehaviour
             moveDirection = new Vector3(moveX, 0, moveZ);
             moveDirection *= moveSpeed;
 
-            if (Input.GetKey(KeyCode.LeftShift) && moveZ == 1)
-            {
-                moveSpeed = sprintSpeed;
-            }
-            else
-            {
-                moveSpeed = walkSpeed;
-            }
+            Sprint(moveZ);
+            Jump();
         }
 
         moveDirection.y -= gravity;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDirection.y += jumpSpeed;
+        }
+    }
+
+    private void Sprint(float moveZ)
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && moveZ == 1)
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
     }
 }
